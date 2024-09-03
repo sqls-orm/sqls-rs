a14b9f1f2bac87fc65b4c1e2ff54bb5320f0fa5e
cxllmerichie <cxllmerichie@gmail.com>
2024-09-03 18:20:12 +0300

    latest

---
+++ builder/tests/test_sqlx.rs
@@ -0,0 +1,39 @@
+use builder as sql;
+use builder::Model;
+use model::Model;
+use optional::Optional;
+
+mod models;
+
+#[derive(Optional, Model)]
+struct User {
+    pub id: u8,
+    pub username: String,
+    pub password: String,
+}
+
+#[tokio::test]
+async fn select_distinct_count() {
+    dotenv::dotenv().ok();
+
+    sqlx::query_as::<_, UserOptional>(r#"
+         SELECT username
+         FROM user
+         WHERE id = %s
+    "#)
+        .bind(0u8)
+        .fetch_optional()
+        .await
+        .unwrap_or_else(|e| {
+            println!("{}", e.to_string());
+            None
+        });
+
+    let (query, args) = sql::select(vec![
+        models::User::username().distinct(),
+        models::User::password().count().as_("total"),
+    ])
+        .from(models::User::table())
+        .build();
+    println!("{} {:?}", query, args);
+}
